34aaf1500cbd12f37d81be5907c2a7d2b7f2fb3a
anasbusytech <anas24.busytech@gmail.com>
2025-10-22 20:37:05 +0500

    thankyou page update

--- extensions/thankyou-page-customize/src/ThankYouPageSurvey.jsx
+++ extensions/thankyou-page-customize/src/ThankYouPageSurvey.jsx
@@ -9,10 +9,17 @@ function Attribution() {
   return (
     <Survey
       title="Important Notice ⚠️"
-      description={`If you don't attend our confirmation call, your order will be automatically canceled within 3 days.
-Please ensure your contact number is correct and active.
-
-For order confirmation or assistance, contact us at +92 304 2965682.`}
+      description={
+        <>
+          If you don't attend our confirmation call, your order will be automatically canceled within 3 days. Please ensure your contact number is correct and active.
+          <br />
+          <br />
+          For order confirmation or assistance, contact us at{" "}
+          <a href="tel:+923042965682" style={{ color: "#007bff", textDecoration: "underline" }}>
+            +92 304 2965682
+          </a>.
+        </>
+      }
     />
   );
 }
